a14d1023f57d0301a5a874eee74442eb4acd317e
pecavalc <pecavalc@student.42berlin.de>
2026-03-16 10:49:47 +0100

    Rename model to parsed_model

--- includes/parsed_model.c
+++ includes/parsed_model.c
@@ -6,17 +6,17 @@
 /*   By: pecavalc <pecavalc@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/16 10:10:51 by pecavalc          #+#    #+#             */
-/*   Updated: 2026/03/16 10:37:10 by pecavalc         ###   ########.fr       */
+/*   Updated: 2026/03/16 10:49:19 by pecavalc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parsed_model.h"
 
-void	parsed_model_create(t_parsed_model *model)
+void	parsed_model_create(t_parsed_model *parsed_model)
 {
 	// TODO
 }
-void	parsed_model_destroy(t_parsed_model *model)
+void	parsed_model_destroy(t_parsed_model *parsed_model)
 {
 	// TODO
 }
