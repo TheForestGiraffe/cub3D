4cca8e759992f24fa38005be1680c589bce643ea
Pedro Lima <pedrolima.ep@gmail.com>
2026-03-16 20:33:32 +0100

    Rename parsed_model

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: pecavalc <pecavalc@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/10 19:00:41 by tcunha            #+#    #+#             */
-/*   Updated: 2026/03/16 13:50:07 by pecavalc         ###   ########.fr       */
+/*   Updated: 2026/03/16 20:33:20 by pecavalc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,16 @@
 
 int	main(int argc, char **argv)
 {
-	t_parsed_model	parsed_model;
-	t_game			game;
+	t_level_description	level_description;
+	t_game				game;
 
-	if (parse(argc, argv, &parsed_model))
+	if (parse(argc, argv, &level_description))
 		return (1);
-	if (game_setup(&game, &parsed_model))
+	if (game_setup(&game, &level_description))
 	{
-		parsed_model_destroy(&parsed_model);
+		parsed_model_destroy(&level_description);
 		return (1);
 	}
-	parsed_model_destroy(&parsed_model);	
+	parsed_model_destroy(&level_description);	
 	return (0);
 }
