df6b6daff1d899acc66ad10194f41f50eb51f5e0
pecavalc <pecavalc@student.42berlin.de>
2026-03-16 13:51:46 +0100

    Implement main up to game_setup

--- src/main.c
+++ src/main.c
@@ -6,19 +6,25 @@
 /*   By: pecavalc <pecavalc@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/10 19:00:41 by tcunha            #+#    #+#             */
-/*   Updated: 2026/03/16 13:11:24 by pecavalc         ###   ########.fr       */
+/*   Updated: 2026/03/16 13:50:07 by pecavalc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
+#include "game.h"
 
 int	main(int argc, char **argv)
 {
-	t_parsed_model	*parsed_model;
+	t_parsed_model	parsed_model;
+	t_game			game;
 
-	parsed_model = parse(argc, argv);
-	if (!parsed_model)
+	if (parse(argc, argv, &parsed_model))
 		return (1);
-	
+	if (game_setup(&game, &parsed_model))
+	{
+		parsed_model_destroy(&parsed_model);
+		return (1);
+	}
+	parsed_model_destroy(&parsed_model);	
 	return (0);
 }
