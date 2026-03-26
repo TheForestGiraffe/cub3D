5b2a91f2738e1c7b15c2ba6776c2ddbb6983c1b4
pecavalc <pecavalc@student.42berlin.de>
2026-03-26 17:57:14 +0100

    Load player: WIP

--- src/game/game_setup.c
+++ src/game/game_setup.c
@@ -6,7 +6,7 @@
 /*   By: pecavalc <pecavalc@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/16 20:34:35 by pecavalc          #+#    #+#             */
-/*   Updated: 2026/03/20 21:58:32 by pecavalc         ###   ########.fr       */
+/*   Updated: 2026/03/26 17:56:59 by pecavalc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,35 @@ static void	player_load(t_game *game, t_map *map)
 {
 	game->player.x = map->player.x + 0.5;
 	game->player.y = map->player.y + 0.5;
-	
+	if (map->player.direction == 'N')
+	{
+		game->player.dir_x = 0;
+		game->player.dir_y = -1;
+		game->player.plane_x = 0.66;
+		game->player.plane_y = 0;
+		
+	}
+	else if (map->player.direction == 'S')
+	{
+		game->player.dir_x = 0;
+		game->player.dir_y = 1;
+		game->player.plane_x = -0.66;
+		game->player.plane_y = 0;
+	}
+	else if (map->player.direction == 'E')
+	{
+		game->player.dir_x = 1;
+		game->player.dir_y = 0;
+		game->player.plane_x = 0;
+		game->player.plane_y = 0.66;
+	}
+	else
+	{
+		game->player.dir_x = -1;
+		game->player.dir_y = 0;
+		game->player.plane_x = 0;
+		game->player.plane_y = -0.66;
+	}
 }
 
 static void	game_init(t_game *game)
