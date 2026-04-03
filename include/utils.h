9b45291d884944257c7cdf758d13f3cb4dd3549a
pecavalc <pecavalc@student.42berlin.de>
2026-04-03 15:18:57 +0200

    Fix utils.h - revert to state as in the main branch

--- include/utils.h
+++ include/utils.h
@@ -1,30 +1,10 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-<<<<<<<< HEAD:include/mlx_wrapper.h
-/*   mlx_wrapper.h                                      :+:      :+:    :+:   */
-========
 /*   utils.h                                            :+:      :+:    :+:   */
->>>>>>>> origin/main:include/utils.h
 /*                                                    +:+ +:+         +:+     */
 /*   By: tcunha <tcunha@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-<<<<<<<< HEAD:include/mlx_wrapper.h
-/*   Created: 2026/03/17 21:46:12 by pecavalc          #+#    #+#             */
-/*   Updated: 2026/03/20 20:59:53 by pecavalc         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#ifndef MLX_WRAPPER_H
-# define MLX_WRAPPER_H
-
-# include "game.h"
-
-int		mlx_setup(t_game *game);
-void	mlx_destroy(t_mlx *mlx);
-
-#endif
-========
 /*   Created: 2026/03/26 19:50:13 by tcunha            #+#    #+#             */
 /*   Updated: 2026/03/26 19:50:45 by tcunha           ###   ########.fr       */
 /*                                                                            */
@@ -36,4 +16,3 @@ void	mlx_destroy(t_mlx *mlx);
 void	print_error(char *file, char *function);
 
 #endif
->>>>>>>> origin/main:include/utils.h
