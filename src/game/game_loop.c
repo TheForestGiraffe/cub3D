fa8059dec7a5d962acb1ead23a55e404eb85ebc4
pecavalc <pecavalc@student.42berlin.de>
2026-04-03 19:43:17 +0200

    Remove temp folder as its contents are not needed anymore.

--- src/game/game_loop.c
+++ src/game/game_loop.c
@@ -6,7 +6,7 @@
 /*   By: pecavalc <pecavalc@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/20 17:01:37 by pecavalc          #+#    #+#             */
-/*   Updated: 2026/03/27 00:04:36 by pecavalc         ###   ########.fr       */
+/*   Updated: 2026/04/03 19:41:56 by pecavalc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,15 @@
 #include <unistd.h>
 #include <stdio.h>
 
+// TODO: Remove test print
+
 int	game_loop(void *param)
 {
 	t_game	*game;
 
 	game = (t_game *)param;
-	(void)game;
-	printf("game_loop running\n");
+	cast_rays(game);
+	printf("game_loop() running\n");
 	usleep(1000000);
 	return (0);
 }
