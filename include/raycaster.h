1e745e473a39c9879d1f39b140e49e6b28807865
pecavalc <pecavalc@student.42berlin.de>
2026-04-06 18:17:46 +0200

    WIP: write cast_rays.c

---
+++ include/raycaster.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   raycaster.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pecavalc <pecavalc@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/06 16:53:02 by pecavalc          #+#    #+#             */
+/*   Updated: 2026/04/06 16:57:41 by pecavalc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef RAYCASTER_H
+# define RAYCASTER_H
+
+typedef struct s_ray
+{
+
+}	t_ray;
+
+t_ray	*cast_rays(t_game *game);
+
+#endif
