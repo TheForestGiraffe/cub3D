c16e3d9ae058a3de1ff94f6639ae6875dc65d670
pecavalc <pecavalc@student.42berlin.de>
2026-04-10 12:21:43 +0200

    Register hooks and event handlers for player movement controls

--- src/mlx_wrapper/mlx_register_hooks.c
+++ src/mlx_wrapper/mlx_register_hooks.c
@@ -6,7 +6,7 @@
 /*   By: pecavalc <pecavalc@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/16 10:55:28 by pecavalc          #+#    #+#             */
-/*   Updated: 2026/04/03 20:08:02 by pecavalc         ###   ########.fr       */
+/*   Updated: 2026/04/10 12:14:55 by pecavalc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,6 @@ void	mlx_register_hooks(t_game *game)
 	mlx = &game->mlx;
 	mlx_hook(mlx->window, DestroyNotify, 0, on_window_destroy, game);
 	mlx_hook(mlx->window, KeyPress, KeyPressMask, on_key_press, game);
+	mlx_hook(mlx->window, KeyRelease, KeyReleaseMask, on_key_release, game);
 	mlx_loop_hook(mlx->mlx, game_loop, game);
 }
