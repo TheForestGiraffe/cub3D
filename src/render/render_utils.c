31a83112559b8b13205e8ac07cb9df7752bd4c23
Pedro Lima <pedrolima.ep@gmail.com>
2026-04-11 08:33:46 +0200

    Print error when trying to put_pixel out of boundaries

--- src/render/render_utils.c
+++ src/render/render_utils.c
@@ -3,21 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   render_utils.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tcunha <tcunha@student.42berlin.de>        +#+  +:+       +#+        */
+/*   By: pecavalc <pecavalc@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/06 20:33:10 by tcunha            #+#    #+#             */
-/*   Updated: 2026/04/10 18:39:07 by tcunha           ###   ########.fr       */
+/*   Updated: 2026/04/11 08:22:57 by pecavalc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "game.h"
+#include "utils.h"
 
 int	img_put_pixel(t_game *game, int x, int y, unsigned int color)
 {
 	char	*pixel;
 
 	if (x < 0 || x >= game->mlx.width || y < 0 || y >= game->mlx.height)
-		return (1);
+		return (print_error("@put_pixel", "x or y out of screen bounds"), 1);
 	pixel = game->mlx.img.address + y * game->mlx.img.line_size + x
 		* (game->mlx.img.bits_per_pixel / 8);
 	*(unsigned int *)pixel = color;
